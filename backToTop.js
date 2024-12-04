00ce9a1b79634317d30bd9ee3d142e1cb1c9a527
CNX-PATTANA <npgm.usa.studio@gmail.com>
2024-12-04 11:13:25 +0700

    Create backToTop.js

---
+++ backToTop.js
@@ -0,0 +1,7 @@
+document.getElementById('backToTop').addEventListener('click', function (event) {
+    event.preventDefault(); 
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+});
